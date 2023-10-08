1f22562ce83978d68fcc6b52db6c64c7598e8fa8
iamaseda <asedaasomani@gmail.com>
2023-10-08 04:30:57 -0400

    Menus no longer causing hook call errors. Display properly.

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import * as React from "react";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-// import Button from "@material-ui/core/Button";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 function App() {
+
+  const [anchorEl1, setAnchorEl1] = React.useState(null);
+  const [anchorEl2, setAnchorEl2] = React.useState(null);
+
+  const open1 = Boolean(anchorEl1);
+  const open2 = Boolean(anchorEl2);
+
+  const handleClick1 = (event) => {
+    setAnchorEl1(event.currentTarget);
+  };
+  const handleClick2 = (event) => {
+    setAnchorEl2(event.currentTarget);
+  };
+
+  const handleClose1 = () => {
+    setAnchorEl1(null);
+  };
+  const handleClose2 = () => {
+    setAnchorEl2(null);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,11 +42,62 @@ function App() {
         >
           Learn React Aaaaaahhhhhh!!!
         </a>
-        {/* <Menu>
+        <Button
+         id="basic-button"
+         aria-controls={open2 ? 'basic-menu' : undefined}
+         aria-haspopup="true"
+         aria-expanded={open2 ? 'true' : undefined}
+         onClick={handleClick2}>
+          SideMenu
+        </Button>
+        <Menu
+          id="basic-menu"
+          aria-labelledby="demo-positioned-button"
+          anchorEl={anchorEl2}
+          open={open2}
+          onClose={handleClose2}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}>
           <MenuItem>Land</MenuItem>
           <MenuItem>Housing</MenuItem>
           <MenuItem>Miscellaneous</MenuItem>
-        </Menu> */}
+        </Menu>
+        <p>Check 1</p>
+
+         <Button
+        id="demo-positioned-button"
+        aria-controls={open1 ? 'demo-positioned-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open1 ? 'true' : undefined}
+        onClick={handleClick1}
+      >
+        Dashboard
+      </Button>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl1}
+        open={open1}
+        onClose={handleClose1}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem onClick={handleClose1}>Profile</MenuItem>
+        <MenuItem onClick={handleClose1}>My account</MenuItem>
+        <MenuItem onClick={handleClose1}>Logout</MenuItem>
+      </Menu>
       </header>
     </div>
   );
