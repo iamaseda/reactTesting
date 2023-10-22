c09996061de769c793e62e1a6abeefa08b0eca72
iamaseda <asedaasomani@gmail.com>
2023-10-22 17:58:00 -0400

    Saving changes before importing babylonjs scene component

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -4,6 +4,11 @@ import * as React from "react";
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import {Box, Drawer, List, ListItem, ListItemButton,
+  ListItemIcon, ListItemText, Divider} from '@mui/material';
+  import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
 
 function App() {
 
@@ -27,10 +32,72 @@ function App() {
     setAnchorEl2(null);
   };
 
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const boxDefault = {
+    height: 100,
+    border: '1px solid black',
+    padding: 2,
+    minWidth: 600,
+    m: 1
+  };
+
+  const boxStyle = {
+    marginLeft: 1
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} width={100} height={100} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -42,34 +109,10 @@ function App() {
         >
           Learn React Aaaaaahhhhhh!!!
         </a>
-        <Button
-         id="basic-button"
-         aria-controls={open2 ? 'basic-menu' : undefined}
-         aria-haspopup="true"
-         aria-expanded={open2 ? 'true' : undefined}
-         onClick={handleClick2}>
-          SideMenu
-        </Button>
-        <Menu
-          id="basic-menu"
-          aria-labelledby="demo-positioned-button"
-          anchorEl={anchorEl2}
-          open={open2}
-          onClose={handleClose2}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-          }}>
-          <MenuItem>Land</MenuItem>
-          <MenuItem>Housing</MenuItem>
-          <MenuItem>Miscellaneous</MenuItem>
-        </Menu>
+        <br></br>
+        
         <p>Check 1</p>
-
+          
          <Button
         id="demo-positioned-button"
         aria-controls={open1 ? 'demo-positioned-menu' : undefined}
@@ -99,6 +142,54 @@ function App() {
         <MenuItem onClick={handleClose1}>Logout</MenuItem>
       </Menu>
       </header>
+      <Box
+          m={1}//margin
+          display="flex"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          sx={{width: '30%', border: 1}}
+          style={boxStyle}
+        >
+        <Button
+         id="basic-button"
+         aria-controls={open2 ? 'basic-menu' : undefined}
+         aria-haspopup="true"
+         aria-expanded={open2 ? 'true' : undefined}
+         onClick={handleClick2}
+         >
+          SideMenu
+        </Button>
+        </Box>
+        <Menu
+          id="basic-menu"
+          aria-labelledby="demo-positioned-button"
+          anchorEl={anchorEl2}
+          open={open2}
+          onClose={handleClose2}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}>
+          <MenuItem>Land</MenuItem>
+          <MenuItem>Housing</MenuItem>
+          <MenuItem>Miscellaneous</MenuItem>
+        </Menu>
+      {['left', 'right', 'top', 'bottom'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
     </div>
   );
 }
